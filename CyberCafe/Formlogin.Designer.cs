b89458691ea7a35ef2ecd5045990f4b984eb5bbc
Danh Nguyễn <danh542004@gmail.com>
2024-12-18 18:18:46 +0700

    message

---
+++ CyberCafe/Formlogin.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace CyberCafe
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.lbl_Login = new System.Windows.Forms.Label();
+            this.txt_Username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.grb_PleaseLogin = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.grb_PleaseLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Login.Location = new System.Drawing.Point(142, 208);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(135, 50);
+            this.btn_Login.TabIndex = 0;
+            this.btn_Login.Text = "LOGIN";
+            this.btn_Login.UseVisualStyleBackColor = false;
+            // 
+            // lbl_Login
+            // 
+            this.lbl_Login.AutoSize = true;
+            this.lbl_Login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lbl_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Login.Location = new System.Drawing.Point(289, 119);
+            this.lbl_Login.Name = "lbl_Login";
+            this.lbl_Login.Size = new System.Drawing.Size(343, 46);
+            this.lbl_Login.TabIndex = 1;
+            this.lbl_Login.Text = "CYBER COFFEE";
+            // 
+            // txt_Username
+            // 
+            this.txt_Username.BackColor = System.Drawing.Color.Silver;
+            this.txt_Username.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_Username.Location = new System.Drawing.Point(97, 65);
+            this.txt_Username.Name = "txt_Username";
+            this.txt_Username.Size = new System.Drawing.Size(299, 26);
+            this.txt_Username.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(92, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "User Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(92, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.BackColor = System.Drawing.Color.Silver;
+            this.txt_Password.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_Password.Location = new System.Drawing.Point(97, 158);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(299, 26);
+            this.txt_Password.TabIndex = 5;
+            // 
+            // grb_PleaseLogin
+            // 
+            this.grb_PleaseLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.grb_PleaseLogin.Controls.Add(this.pictureBox1);
+            this.grb_PleaseLogin.Controls.Add(this.txt_Password);
+            this.grb_PleaseLogin.Controls.Add(this.label3);
+            this.grb_PleaseLogin.Controls.Add(this.btn_Login);
+            this.grb_PleaseLogin.Controls.Add(this.label2);
+            this.grb_PleaseLogin.Controls.Add(this.txt_Username);
+            this.grb_PleaseLogin.Location = new System.Drawing.Point(254, 221);
+            this.grb_PleaseLogin.Name = "grb_PleaseLogin";
+            this.grb_PleaseLogin.Size = new System.Drawing.Size(421, 278);
+            this.grb_PleaseLogin.TabIndex = 6;
+            this.grb_PleaseLogin.TabStop = false;
+            this.grb_PleaseLogin.Text = "Please login";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(362, 158);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 26);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CyberCafe.Properties.Resources.images;
+            this.pictureBox2.Location = new System.Drawing.Point(91, 42);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(742, 557);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 744);
+            this.Controls.Add(this.grb_PleaseLogin);
+            this.Controls.Add(this.lbl_Login);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "FormLogin";
+            this.Text = "Form Login";
+            this.grb_PleaseLogin.ResumeLayout(false);
+            this.grb_PleaseLogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Label lbl_Login;
+        private System.Windows.Forms.TextBox txt_Username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.GroupBox grb_PleaseLogin;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
